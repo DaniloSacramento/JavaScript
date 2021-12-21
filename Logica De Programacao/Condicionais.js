2d393d9d24f0fbfbad81001f2d1de05524520efa
Caroline Correia Nogueira <caroline_nc@MacBook-Air-de-Caroline.local>
2021-12-21 11:04:00 -0300

    Condicionais

---
+++ Logica De Programacao/Condicionais.js
@@ -0,0 +1,9 @@
+let velocity = 90
+
+if (velocity < 100) {
+    console.log("estamos muito devagar")
+} else if (velocity <= 100) {
+    console.log("Cuidado! Quase na velocidade de risco")
+} else {
+    console.log("Ja estamos em velocidade de risco!")
+}
