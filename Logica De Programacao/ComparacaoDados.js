18e49e44e10453477d3119424954903699861cdc
Caroline Correia Nogueira <caroline_nc@MacBook-Air-de-Caroline.local>
2021-12-21 08:45:55 -0300

    fazendo mais alteracoes

--- Logica De Programacao/ComparacaoDados.js
+++ Logica De Programacao/ComparacaoDados.js
@@ -2,3 +2,5 @@ let spaceship = "Golias"
 let velocidade = 90
 
 console.log(velocidade == 100)
+
+console.log(spaceship == "Golias")
---
+++ Logica De Programacao/TabelaVerdade.js
@@ -0,0 +1,10 @@
+let spaceship = "Elemental"
+let velocity = 20
+
+// E = &&
+
+// OU = || 
+
+// NAO = ! 
+
+console.log(!(velocidade > 25 && spaceship == "Elemental") || (velocity - 3 == 17 && spaceship.length + 1 > 15))
